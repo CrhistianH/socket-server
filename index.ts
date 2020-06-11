b5dd24604dd91952852f5242b1b0b48ba4dcd262
Crhistian Higareda <crhistianhigareda@gmail.com>
2020-06-11 17:55:28 -0500

    seccion 4

--- index.ts
+++ index.ts
@@ -3,14 +3,14 @@ import ROUTER from "./routes/router";
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
-const SERVER = new Server();
+const SERVER = Server.instance;
 
 // Body Parser
 SERVER.app.use(bodyParser.urlencoded({extended: true}));
 SERVER.app.use(bodyParser.json());
 
 // CORS
-SERVER.app.use( cors({origin: true, credentials: true}))
+SERVER.app.use(cors({origin: true, credentials: true}))
 
 // Rutas de servicios
 SERVER.app.use('/', ROUTER)
